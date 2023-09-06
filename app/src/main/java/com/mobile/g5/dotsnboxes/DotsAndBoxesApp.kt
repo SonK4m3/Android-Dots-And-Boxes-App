5a506f42d6f905ec88729244e100133d15cb9a87
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2023-09-07 04:48:16 +0700

    Update: add appModule and application hilt

---
+++ app/src/main/java/com/mobile/g5/dotsnboxes/DotsAndBoxesApp.kt
@@ -0,0 +1,7 @@
+package com.mobile.g5.dotsnboxes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DotsAndBoxesApp: Application()
---
+++ app/src/main/java/com/mobile/g5/dotsnboxes/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.mobile.g5.dotsnboxes.di
+
+import com.mobile.g5.dotsnboxes.data.KtorRealtimeMessagingClient
+import com.mobile.g5.dotsnboxes.data.RealtimeMessagingClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.websocket.WebSockets
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(CIO) {
+            install(Logging)
+            install(WebSockets)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRealtimeMessagingClient(httpClient: HttpClient): RealtimeMessagingClient {
+        return KtorRealtimeMessagingClient(httpClient)
+    }
+}
