55c21ce1f03b8be695ab76e4f2b310869196b91c
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2023-09-07 04:46:34 +0700

    Update: Board UI

--- app/src/main/java/com/mobile/g5/dotsnboxes/ui/theme/Color.kt
+++ app/src/main/java/com/mobile/g5/dotsnboxes/ui/theme/Color.kt
@@ -7,9 +7,17 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val GrayBackground = Color(0xFFf5f7f6)
 val BlueCustom = Color(0xFF3871df)
-val OrangeRed = Color(0xFFF6635C)
 val Blue01 = Color(0xFF337CCF)
-val Beige = Color(0xFFFFF5E0)
+
+val Beige = Color(0xFFF2ECBE)
+
+val Orange01 = Color(0xFFFF8551)
+
+val GrayBackground = Color(0xFFf5f7f6)
 val Black01 = Color(0xFF393E46)
+
+val Red01 = Color(0xFFDD5353)
+val Red02 = Color(0xFFB73E3E)
+
+val Brown01 = Color(0xFF482121)
